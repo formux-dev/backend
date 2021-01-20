7ae8d30f9e2c562c5ae9516193d87d976e085407
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2021-01-20 12:41:01 +0100

    Only show forms with title

--- src/routes/getForms.js
+++ src/routes/getForms.js
@@ -5,12 +5,14 @@ async function getForms(request, response) {
     const forms = await firestore.collection("forms").get();
 
     response.send(
-      forms.docs.map(doc => {
-        return {
-          formId: doc.id,
-          title: doc.data().meta.title,
-        };
-      })
+      forms.docs
+        .filter(doc => doc.data().meta && doc.data().meta.title)
+        .map(doc => {
+          return {
+            formId: doc.id,
+            title: doc.data().meta.title,
+          };
+        })
     );
   } catch ({ message }) {
     response.status(500).send({ message });
