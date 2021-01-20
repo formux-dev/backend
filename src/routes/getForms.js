1c73094cd759b52edc6d63fe33107c72ebb490fc
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2021-01-20 10:51:34 +0100

    Send forms directly

--- src/routes/getForms.js
+++ src/routes/getForms.js
@@ -4,14 +4,14 @@ async function getForms(request, response) {
   try {
     const forms = await firestore.collection("forms").get();
 
-    response.send({
-      forms: forms.docs.map(doc => {
+    response.send(
+      forms.docs.map(doc => {
         return {
           id: doc.id,
           title: doc.data().meta.title,
         };
-      }),
-    });
+      })
+    );
   } catch ({ message }) {
     response.status(500).send({ message });
   }
